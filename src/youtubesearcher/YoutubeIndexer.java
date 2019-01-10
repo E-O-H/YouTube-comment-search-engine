9bbf815c6abf1a7898dabb5a3e229c21630c7900
Chenyang Tang <ct1856@nyu.edu>
2019-01-10 04:32:07 -0500

    checkpoint

--- src/youtubesearcher/YoutubeIndexer.java
+++ src/youtubesearcher/YoutubeIndexer.java
@@ -1,8 +1,18 @@
 package youtubesearcher;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 
@@ -18,9 +28,16 @@ import com.google.gson.JsonParser;
  */
 public class YoutubeIndexer {
   
+  Path indexDir = Paths.get("index");
+  
   private static final String URL_BASE = "https://www.googleapis.com/youtube/v3";
   private static final String API_KEY = "AIzaSyDF7H_kAHJsIhijiIKU9cxZuK7sforZnIc";
-  private static final String ACCESS_TOKEN = "636583674247-h0ku9u1erb78r7d7bdg4c1bf0ckj8v70.apps.googleusercontent.com";
+  
+  /*
+   * Lucene indexer internal objects
+   */
+  private StandardAnalyzer analyzer; // analyzer for tokenizing text
+  private Directory index;           // the index
   
   enum Scope {
     VIDEO, CHANNEL;
@@ -29,10 +46,10 @@ public class YoutubeIndexer {
   /**
    * Download the comments of a video
    * 
-   * @param videoId video id
+   * @param scopeId ID of scope (e.g. VideoId for VIDEO scope, ChannelId for CHANNEL scope)
    * @return JSON object of the comments
    */
-  public static JsonArray downloadComments(Scope scope, String videoId) {
+  public static JsonArray downloadComments(Scope scope, String scopeId) {
     final String SCOPE_FILTER;
     switch (scope) {
       case VIDEO:
@@ -48,7 +65,7 @@ public class YoutubeIndexer {
     
     String urlStr = URL_BASE + "/commentThreads" + "?key=" + API_KEY 
                     + "&textFormat=plainText&part=snippet" + SCOPE_FILTER
-                    + videoId + "&maxResults=100";
+                    + scopeId + "&maxResults=100";
     
     Connection.Response response;
     try {
@@ -70,15 +87,87 @@ public class YoutubeIndexer {
     return commentsArrayObj;
   }
   
-  /**
-   * 
-   */
-  public YoutubeIndexer() {
-    // TODO Auto-generated constructor stub
+  private static void addDoc(IndexWriter indexWriter, String commentId, String parentId, 
+                             String userId, String userName, String profilePicture, 
+                             String videoId, int likeCount, int replyCount, 
+                             String comment, String publishTime, String updateTime) throws IOException {
+    Document doc = new Document();
+    
+    
+    
+    
+    //doc.add(new TextField("title", title, Field.Store.YES));
+    //doc.add(new TextField("text", text, Field.Store.YES));
+    //doc.add(new StringField("path", path, Field.Store.YES));
+    
+    //Term key = new Term("id", id);
+    //indexWriter.addDocument(doc);
+  }
+  
+  private void initialize() {
+    try {
+      index = FSDirectory.open(indexDir);
+    } catch (IOException e) {
+      System.err.println("Error opening index directory" + indexDir);
+      e.printStackTrace();
+    }
+    analyzer = new StandardAnalyzer();
+  }
+  
+  public void buildCommentIndex(Scope scope, String scopeId) {
+    initialize();
+    JsonArray topLevelComments = downloadComments(scope, scopeId);
+    for (int i = 0; i < topLevelComments.size(); ++i) {
+      String commentId = topLevelComments.get(i).getAsJsonObject()
+                                         .get("id").getAsString();
+      String videoId = topLevelComments.get(i).getAsJsonObject()
+                                       .get("snippet").getAsJsonObject()
+                                       .get("videoId").getAsString();
+      String userName = topLevelComments.get(i).getAsJsonObject()
+                                        .get("snippet").getAsJsonObject()
+                                        .get("topLevelComment").getAsJsonObject()
+                                        .get("snippet").getAsJsonObject()
+                                        .get("authorDisplayName").getAsString();
+      String profilePicture = topLevelComments.get(i).getAsJsonObject()
+                                              .get("snippet").getAsJsonObject()
+                                              .get("topLevelComment").getAsJsonObject()
+                                              .get("snippet").getAsJsonObject()
+                                              .get("authorProfileImageUrl").getAsString();
+      String userId = topLevelComments.get(i).getAsJsonObject()
+                                      .get("snippet").getAsJsonObject()
+                                      .get("topLevelComment").getAsJsonObject()
+                                      .get("snippet").getAsJsonObject()
+                                      .get("authorChannelId").getAsJsonObject()
+                                      .get("value").getAsString();
+      String comment = topLevelComments.get(i).getAsJsonObject()
+                                       .get("snippet").getAsJsonObject()
+                                       .get("topLevelComment").getAsJsonObject()
+                                       .get("snippet").getAsJsonObject()
+                                       .get("textDisplay").getAsString();
+      String likeCount = topLevelComments.get(i).getAsJsonObject()
+                                         .get("snippet").getAsJsonObject()
+                                         .get("topLevelComment").getAsJsonObject()
+                                         .get("snippet").getAsJsonObject()
+                                         .get("likeCount").getAsString();
+      String publishTime = topLevelComments.get(i).getAsJsonObject()
+                                           .get("snippet").getAsJsonObject()
+                                           .get("topLevelComment").getAsJsonObject()
+                                           .get("snippet").getAsJsonObject()
+                                           .get("publishedAt").getAsString();
+      String updateTime = topLevelComments.get(i).getAsJsonObject()
+                                          .get("snippet").getAsJsonObject()
+                                          .get("topLevelComment").getAsJsonObject()
+                                          .get("snippet").getAsJsonObject()
+                                          .get("updatedAt").getAsString();
+      String replyCount = topLevelComments.get(i).getAsJsonObject()
+                                          .get("snippet").getAsJsonObject()
+                                          .get("totalReplyCount").getAsString();
+    }
   }
   
   public static void main(String[] args) {
-    System.out.println(downloadComments(Scope.CHANNEL, "UC6ReSe57JF9ju3JPYRwxTUQ"));
+    YoutubeIndexer youtubeIndexer = new YoutubeIndexer();
+    youtubeIndexer.buildCommentIndex(Scope.CHANNEL, "UC6ReSe57JF9ju3JPYRwxTUQ");
   }
 
 }
